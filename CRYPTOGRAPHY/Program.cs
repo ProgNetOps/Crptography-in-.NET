f8c9142568b4bfe075596e984a5bf1288c93f4dc
Suberu 'Seyi <suberuseyi@gmail.com>
2025-05-05 16:47:48 +0100

    advanced encryption standards implemented

--- CRYPTOGRAPHY/Program.cs
+++ CRYPTOGRAPHY/Program.cs
@@ -3,5 +3,8 @@
 for (int i = 0; i < 10; i++)
 {
     var randomNumber = RandomNumberGenerator.GetBytes(32);
-    Console.WriteLine(Convert.ToBase64String(randomNumber));
+
+    //conversion to base64string is just encoding from one format to a more human-readable format; it is not encryption.
+    var base64String = Convert.ToBase64String(randomNumber);
+    Console.WriteLine(base64String);
 }
