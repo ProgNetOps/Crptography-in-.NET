110bd210b257ff6267488e08072beee07b0a5b8e
Suberu 'Seyi <suberuseyi@gmail.com>
2025-05-05 10:58:57 +0100

    Implementation of cryptography with .NET

---
+++ CRYPTOGRAPHY/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Security.Cryptography;
+
+for (int i = 0; i < 10; i++)
+{
+    var randomNumber = RandomNumberGenerator.GetBytes(32);
+    Console.WriteLine(Convert.ToBase64String(randomNumber));
+}
