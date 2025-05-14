c311640eb7ec3290e250f6084e81468aad284c0a
Suberu 'Seyi <suberuseyi@gmail.com>
2025-05-14 12:49:48 +0100

    Cryptographic Hashing: MD5, SHA1 and SHA2 implemented

---
+++ Hasher/Program.cs
@@ -0,0 +1,50 @@
+﻿using Hasher;
+using System.Text;
+
+const string originalMessage1 = "Original Message to hash ";
+const string originalMessage2 = "Original Message to hash";
+
+Console.WriteLine("Secure HashData Demonstration in .NET");
+Console.WriteLine("-------------------");
+Console.WriteLine();
+Console.WriteLine($"Original Messsage 1 = {originalMessage1}");
+Console.WriteLine($"Original Messsage 2 = {originalMessage2}");
+Console.WriteLine();
+
+
+var md5HashedMessage1 = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(originalMessage1));
+var md5HashedMessage2 = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(originalMessage2));
+
+
+var sha1HashedMessage1 = HashData.ComputeHashSha1(Encoding.UTF8.GetBytes(originalMessage1));
+var sha1HashedMessage2 = HashData.ComputeHashSha1(Encoding.UTF8.GetBytes(originalMessage2));
+
+
+var sha256HashedMessage1 = HashData.ComputeHashSha256(Encoding.UTF8.GetBytes(originalMessage1));
+var sha256HashedMessage2 = HashData.ComputeHashSha256(Encoding.UTF8.GetBytes(originalMessage2));
+
+
+var sha512HashedMessage1 = HashData.ComputeHashSha512(Encoding.UTF8.GetBytes(originalMessage1));
+var sha512HashedMessage2 = HashData.ComputeHashSha512(Encoding.UTF8.GetBytes(originalMessage2));
+
+Console.WriteLine();
+Console.WriteLine("MD5 Hashes");
+Console.WriteLine();
+Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(md5HashedMessage1)}");
+Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(md5HashedMessage2)}");
+Console.WriteLine();
+Console.WriteLine("SHA1 Hashes");
+Console.WriteLine();
+Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha1HashedMessage1)}");
+Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha1HashedMessage2)}");
+Console.WriteLine();
+Console.WriteLine("SHA256 Hashes");
+Console.WriteLine();
+Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha256HashedMessage1)}");
+Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha256HashedMessage1)}");
+Console.WriteLine();
+Console.WriteLine("SHA512 Hashes");
+Console.WriteLine();
+Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha512HashedMessage1)}");
+Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha512HashedMessage1)}");
+Console.WriteLine();
--- RSAClass/obj/Debug/net9.0/RSAClass.AssemblyInfo.cs
+++ RSAClass/obj/Debug/net9.0/RSAClass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSAClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0657cf6c3197e6b90cfa93089b734a56f0ae8ddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c4c963be2c7df707548042a2b01e678e5ef0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSAClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSAClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
